22c8a86b4935ea13a2f2d53c8cf8bbde916bd5a4
ruhee <ruhee.rajwani@gmail.com>
2018-11-05 21:43:44 -0500

    first chrome extension

---
+++ README.md
@@ -0,0 +1,4 @@
+Google Chrome Extension
+
+Followed the [tutorial](https://developer.chrome.com/extensions/getstarted"tutorial") 
+Customized the default and option colors
---
+++ options.js
@@ -0,0 +1,15 @@
+let page = document.getElementById('buttonDiv');
+  const kButtonColors = ['#FFCC00', '#FF99A3', '#8AFF73', '#82D5FF'];
+  function constructOptions(kButtonColors) {
+    for (let item of kButtonColors) {
+      let button = document.createElement('button');
+      button.style.backgroundColor = item;
+      button.addEventListener('click', function() {
+        chrome.storage.sync.set({color: item}, function() {
+          console.log('color is ' + item);
+        })
+      });
+      page.appendChild(button);
+    }
+  }
+  constructOptions(kButtonColors);
